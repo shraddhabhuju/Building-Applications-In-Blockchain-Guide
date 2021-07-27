212e218c7e1035a1a0d3fa334556df782b1cfe70
Shraddha Bhuju <shraddha.bhuju@gmail.com>
2021-07-27 20:17:54 +0545

    ERCtoken with 18 decimals

---
+++ Program3 - ERC20 tokens/contracts/ERC20Token.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20{
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol){
+        
+    }
+    
+}
